d38b489eafe0ea22ac491bc2407b5f6f3dcb410e
ktai.addr <ktai.addr@gmail.com>
2022-03-23 21:45:21 +0900

    修正

--- test2/src/App.js
+++ test2/src/App.js
@@ -2,37 +2,57 @@ import logo from './logo.svg';
 import './App.css';
 import React, {PureComponent, useState} from 'react';
 import ReactDiffViewer from 'react-diff-viewer';
-import { TextareaAutosize } from '@material-ui/core';
+import {Grid, TextareaAutosize} from '@material-ui/core';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 const _oldCode = `
-ウクライナのゼレンスキー大統領のオンライン演説が日本の国会で午後6時から行われ、ゼレンスキー大統領は「アジアで初めてロシアに対する圧力をかけ始めたのが日本です。引き続き継続をお願いします」と語った。
+サイトエンジン株式会社は2008年に設立されたデジタルマーケティング支援会社です。「改善のタネを広める」を企業理念に、よりよい方法を広めるために日々研究しています。
+創業から一貫してデジタルに特化してコンテンツの企画や制作を行っています。データに基づいた科学的アプローチで集客や売上upにつながるデジタルマーケティング戦略を立案し、Webサイト修正からコンテンツの制作まで実行します。作って終わりではなく、アクセス解析や売上データなどを活用した改善施策の提案まで行い、PDCAサイクルを回すのをお手伝いいたします。
 
-【画像】国会でのオンライン演説の様子を見る
+あなたの会社独自のノウハウが蓄積されていくよう、ともに試行錯誤していきます。デジタルマーケティングを理解したパートナーとしてご活用ください。
 
-細田衆院議長のあいさつの後、ゼレンスキー大統領は、岸田総理大臣や衆参両院の議長をはじめとする国会議員に対し、ウクライナ語で語りかけ同時通訳された。
+経営理念
+改善のタネを広める
 
-「本日は私がウクライナの大統領として初めて外国国家元首としてお話しできることを光栄に思います。」と述べた後、「日本が援助の手を差し伸べて下さいました。心から感謝致します。日本の皆さまが苦しい大変な戦争停止のために努力しはじめた。ウクライナの平和の復活に動き始めました。ウクライナだけでなくヨーロッパ・世界にとって重要です。この戦争が終わらない限り安全に感じる人はいないでしょう。
-アジアで初めてロシアに対する圧力をかけ始めたのが日本です。引き続き継続をお願いします」と支援に感謝するとともに、引き続きの支援を要請した。
+うまくいかずに悩んでいる人には解決法を
+うまくいっている人にもより良くするための方法を提供できる会社になる。
+だれもが幸せになれるようにサイトエンジンが見つけた「改善のネタ」をさまざまな人と共有する。
 
-国会で外国首脳がオンライン演説を行うのは初めてで、設備の関係から、本会議場ではなく、議員会館の2つの会議室が使用された。
+ビジョン
+「もっといい」をすべての人に
+
+情報化されていないものをデータにしたり、再編集して、より良い体験を提供する。
+サイトエンジンが得たその手法をさらに別の誰かに提供することで、
+世界中により良い体験を共有していく。
 `;
 const _newCode = `
-ウクライナのゼレンスキー大統領のオンライン演説が日本の国会で午後7時から行われ、ゼレンスキー大統領は「アジアで初めてロシアに対する圧力をかけ始めたのが日本です。引き続き継続をお願いします」と語った。
+サイトエンジン株式会社は2009年に設立されたデジタルマーケティング支援会社です。「改善のタネを広める」を企業理念に、よりよい方法を広めるために日々研究しています。
+創業から一貫してデジタルに特化してコンテンツの企画や制作を行っています。データに基づいた科学的アプローチで集客や売上アップにつながるデジタルマーケティング戦略を立案し、Webサイト修正からコンテンツの制作まで実行します。作って終わりではなく、アクセス解析や売上データなどを活用した改善施策の提案まで行い、PDCAサイクルを回すのをお手伝いいたします。
 
-【画像】国会でのオンライン演説の様子を見る
+あなたの会社独自のノウハウが蓄積されていくよう、ともに試行錯誤していきます。デジタルマーケティングを理解したパートナーとしてご活用下さい。
 
-細田衆院議長のあいさつの後、ゼレンスキー大統領は、岸田総理大臣や衆参両院の議長をはじめとする国会議員に対し、ウクライナ語で語りかけ同時通訳された。
+経営理念
+改善のタネを広める
 
-「本日は私がウクライナの大統領として初めて外国国家元首としてお話しできることを光栄に思います。」と述べた後、「日本が援助の手を差し伸べて下さいました。心から感謝致します。日本の皆さまが苦しい大変な戦争停止のために努力しはじめた。ウクライナの平和の復活に動き始めました。ウクライナだけでなくヨーロッパ・世界にとって重要です。この戦争が終わらない限り安全に感じる人はいないでしょう。
-アジアで初めてロシアに対する圧力をかけ始めたのが日本です。引き続き継続をお願いします」と支援に感謝するとともに、引き続きの支援を要請した。
+うまくいかずに悩んでいる人には解決法を
+うまくいっている人にもより良くするための方法を提供できる会社になる。
+だれもが幸せになれるようにサイトエンジンが見つけた「改善のタネ」をさまざまな人と共有する。
 
-国会で外国首脳がオンライン演説を行うのは初めてで、設備の関係から、本会議場ではなく、議員会館の3つの会議室が使用された。
+ビジョン
+「もっと良い」をすべての人に
+
+情報化されていないものをデータにしたり、再編集して、より良い体験を提供する。
+サイトエンジンが得たその手法をさらに別の誰かに提供することで、
+世界中により良い体験を共有していく。
 `;
 
 
 function App() {
     const [oldCode,setOldCode] = useState(_oldCode)
-    const [newCode,setNewCode] = useState(_oldCode)
+    const [newCode,setNewCode] = useState(_newCode)
 
     const changeOld = (e)=>{
         setOldCode(e.target.value)
@@ -42,19 +62,33 @@ function App() {
     }
     return (
         <div className="App">
+            <React.Fragment>
+                <CssBaseline />
+                <Container maxWidth="lg">
 
-            <TextareaAutosize aria-label="minimum height" minRows={20} placeholder={oldCode} value={oldCode} style={{width:700}} onChange={changeOld}/>
-            <TextareaAutosize aria-label="minimum height" minRows={20} placeholder={newCode} value={newCode} style={{width:700}} onChange={changeNew}/>
-            <ReactDiffViewer
-                oldValue={oldCode}
-                newValue={newCode}
-                splitView={true}
-                disableWordDiff={false}
-                hideLineNumbers={true}
-                showDiffOnly={false}
-                leftTitle={`変更前`}
-                rightTitle={`変更前`}
-            />
+                    <Grid
+                        container
+                        direction="row"
+                        justifyContent="space-around"
+                        alignItems="center"
+                    >
+                    <TextareaAutosize aria-label="minimum height" minRows={20} placeholder={oldCode} value={oldCode} style={{width:600}} onChange={changeOld}/>
+                    <TextareaAutosize aria-label="minimum height" minRows={20} placeholder={newCode} value={newCode} style={{width:600}} onChange={changeNew}/>
+                    </Grid>
+                    {/*https://github.com/praneshr/react-diff-viewer#props*/}
+                    <ReactDiffViewer
+                        oldValue={oldCode}
+                        newValue={newCode}
+                        splitView={true}
+                        disableWordDiff={false}
+                        hideLineNumbers={true}
+                        showDiffOnly={false}
+                        leftTitle={`変更前`}
+                        rightTitle={`変更前`}
+                        useDarkTheme={false}
+                    />
+                </Container>
+            </React.Fragment>
 
         </div>
     );
